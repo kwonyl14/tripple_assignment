8402860b4a645f858d5497969cef240f6361192e
sqk8657 <sqk8657@naver.com>
2022-06-28 11:18:12 +0900

    Begin: 스프링 부트 환경 셋팅

---
+++ src/test/java/com/tripple/event/TrippleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripple.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
