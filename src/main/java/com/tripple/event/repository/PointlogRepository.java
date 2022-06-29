3885d2287bd38a40264c9cd6d3e7c09811087d70
sqk8657 <sqk8657@naver.com>
2022-06-29 23:53:57 +0900

    Feat: 포인트로그 조회, 삭제 관련 쿼리 추가

--- src/main/java/com/tripple/event/repository/PointlogRepository.java
+++ src/main/java/com/tripple/event/repository/PointlogRepository.java
@@ -4,10 +4,17 @@ import com.tripple.event.entity.Photo;
 import com.tripple.event.entity.Pointlog;
 import com.tripple.event.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.awt.*;
 import java.util.List;
 
 public interface PointlogRepository extends JpaRepository<Pointlog, Long> {
     List<Pointlog> findPointsByReview(Review review);
+
+    @Query("select coalesce(sum(p.value), 0) from Pointlog p join p.review r join p.review.user u where u.id = :userId")
+    int countByUserId(@Param("userId") String userId);
+
+    void deleteByReview(Review review);
 }
