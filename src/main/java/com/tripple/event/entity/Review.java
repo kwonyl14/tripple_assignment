a0d6978fbb5b1a91ab531dee4e7230e2a381788b
sqk8657 <sqk8657@naver.com>
2022-06-29 23:53:16 +0900

    Feat: 롬복 적용, 제약 조건 수정

--- src/main/java/com/tripple/event/entity/Review.java
+++ src/main/java/com/tripple/event/entity/Review.java
@@ -1,5 +1,7 @@
 package com.tripple.event.entity;
 
+import com.tripple.event.request.ReviewUpdateReq;
+import lombok.Getter;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -7,6 +9,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Entity
 @Table(
         uniqueConstraints = {
@@ -26,15 +29,19 @@ public class Review extends BaseEntity implements Persistable<String>{
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "place_ID")
+    @JoinColumn(name = "place_ID", nullable = false)
     private Place place;
 
     @OneToMany(mappedBy = "review")
     private List<Photo> photos = new ArrayList<>();
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
 
     public Review() {
     }
@@ -46,9 +53,9 @@ public class Review extends BaseEntity implements Persistable<String>{
         this.place = place;
     }
 
-    @Override
-    public String getId() {
-        return this.id;
+    public void updateReview(ReviewUpdateReq reviewUpdateReq) {
+        if (reviewUpdateReq.getContent() != null && reviewUpdateReq.getContent().trim().length()>0)
+            this.content = reviewUpdateReq.getContent();
     }
 
     @Override
