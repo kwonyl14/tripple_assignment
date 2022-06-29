0ff8b2564e6ac985a60aea0533715c804ba3d3d7
sqk8657 <sqk8657@naver.com>
2022-06-29 23:51:57 +0900

    Feat: 스웨거 3.0 환경설정 추가

---
+++ src/main/java/com/tripple/event/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.tripple.event.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @FileName : SwaggerConfig
+ * @Class 설명 : 스웨거 설정 클래스
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
